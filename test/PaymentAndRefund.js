479d8cdea0dadf515d6a829e995605979576a0e3
MB <michael.dburke56@gmail.com>
2022-10-05 17:53:58 -0500

    Define test cases as per discussed requirments.

--- test/PaymentAndRefund.js
+++ test/PaymentAndRefund.js
@@ -12,29 +12,75 @@ describe("PaymentAndRefund", function () {
   // Use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
-    const [owner, user1, user2] = await ethers.getSigners();
+    const [admin, user1, user2] = await ethers.getSigners();
 
     const Contract = await ethers.getContractFactory("PaymentAndRefund");
     const instance = await Contract.deploy(USDCAddress);
 
     await instance.deployed();
 
-    return { instance, owner, user1, user2 };
+    return { instance, admin, user1, user2 };
   }
 
-  describe("Deployment", function () {
-    it("===TEMPLATE TEST===", async function () {
-      const { instance, owner } = await loadFixture(deployFixture);
+  describe("Deposit", function () {
+    it("User cannot make multiple deposits", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+
+    it("User can deposite USDC", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+
+    it("Deposites will increment global var ` `", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
 
       expect(true).to.equal(false);
     });
   });
 
   describe("Withdraw", function () {
-    it("===TEMPLATE TEST===", async function () {
-      const { instance, owner } = await loadFixture(deployFixture);
+    it("User can withdraw according the `refundSchedule` (Pull over push)", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+      
+    it("User can be withdrawn from program by administrator (Push payment)", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+  });
+
+  describe("Business logic", function () {
+    it("Updating `refundSchedule` does not effect existing users", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+      
+    it("Payment is for no greater than 30 days in the future", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+
+    it("Payment is for no greater than 21 days in the past", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
 
       expect(true).to.equal(false);
     });
   });
 });
+
+/*
+    it("===TEMPLATE TEST===", async function () {
+      const { instance, admin } = await loadFixture(deployFixture);
+
+      expect(true).to.equal(false);
+    });
+*/
