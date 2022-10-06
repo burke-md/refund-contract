14876bd10ee17da1a27837380da0d0205279da95
MB <michael.dburke56@gmail.com>
2022-10-05 20:20:52 -0500

    Implement config required for mainnet fork testing.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const alchemyKey = process.env.ALCHEMY_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    netoworks: {
+        hardhat: {
+            forking: {
+                url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
+                blockNumber: 15685704
+            }
+        }
+    }
 };
