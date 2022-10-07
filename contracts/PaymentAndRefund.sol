88740fdd7d5dfe4b27232f37552266b16fd7b1a0
MB <michael.dburke56@gmail.com>
2022-10-07 11:37:59 -0500

    Clean up read from state logic.

--- contracts/PaymentAndRefund.sol
+++ contracts/PaymentAndRefund.sol
@@ -6,9 +6,7 @@ pragma solidity 0.8.17;
 
 contract PaymentAndRefund {
 
-    //address private immutable USDC;
-
-    IERC20 usdc;
+    IERC20 USDC;
     // Each index is 1 week. First two weeks 100% refund is available
     uint8[] public refundSchedule = [
         100,
@@ -33,22 +31,6 @@ contract PaymentAndRefund {
     address public admin;
     uint64 constant ONE_WEEK = 3600 * 24 * 7;
 
-    constructor(address _usdc) {
-        admin = msg.sender;
-        //USDC = _usdc;
-
-        usdc = IERC20(_usdc);
-    }
-
-    /******
-    * TODO:
-    * 
-    * - Check logic: currently rounding down (mod operator) when calculating returns
-    * - Resolve mainnet fork USCD testing issue
-    * - Start time (re: discussed limits) array of possible start dates?
-    * - Clarify intended seller withdraw flow and to admin address ok?
-    */
-
     struct Deposit {
         uint64 originalDepositInDollars;  // price may change
         // balanceLeftInDollars to be calculated at time of withdraw?
@@ -57,50 +39,35 @@ contract PaymentAndRefund {
         uint64 startTime;
         uint8[] refundSchedule; // The refund schedule can change in the future, but it should stay with the agreed up on one
     }
-
-    modifier onlyAdmin {
-        require(msg.sender == admin);
-        _;
+//----------------------------------------------------------------------------\\
+    constructor(address _usdc) {
+        admin = msg.sender;
+        USDC = IERC20(_usdc);
     }
 
-    function setPrice(uint64 _price) external onlyAdmin {
-        priceInDollars = _price;
-    }
+    /******
+    * TODO:
+    * 
+    * - Resolve mainnet fork USCD testing issue
+    * - Simplfify admin with draw flow
+    * - Track each students book keep at time of withdraw
+    */
 
+
+//-----------------------------BUYER (student)--------------------------------\\
     function payUpfront(uint64 _price, uint64 _startTime) external {
         uint64 currentPriceInDollars = priceInDollars;
-        // cannot deposit twice
+
         require(deposits[msg.sender].depositTime == 0,
                 "User cannot deposit twice.");
-        // require price to be correct
         require(_price == currentPriceInDollars,
                 "User must pay correct price.");
-
-        /**** VALIDATE _STARTtIME******/
         // require approval granted
-
-        /*
-        bytes memory payload = abi.encodeWithSignature("allowance(address,address)",
-            msg.sender, address(this));
-
-        (bool successCheck, bytes memory returnCheckData) = address(USDC).call(payload);
+        require(USDC.allowance(msg.sender, address(this)) >= currentPriceInDollars * 10 ** 6,
+                "User must have made allowance via USDC contract.");
+                
+        USDC.transferFrom(msg.sender, address(this), currentPriceInDollars * 10 ** 6);
         
-        require(successCheck, "Non-successful allance check.");
-        *
-        *               type issue: how to compare bytes returnCheckData w/ uint64
-        require(uint64(returnCheckData) >= currentPriceInDollars * 10 ** 6,
-                "User must approve sufficient amount of USDC.");
-        *
-        *
-        bytes memory payload2 = abi.encodeWithSignature("transferFrom(address,address,uint256)", 
-            msg.sender, address(this), currentPriceInDollars * 2 * 10 ** 6);
-
-        (bool successTransfer, bytes memory returnTransferData) = address(USDC).call(payload2);
-
-        require(successTransfer, "Non-successful transferFrom operation.");
-        */ 
-
-
         depositedUSDC += currentPriceInDollars;
 
         Deposit memory deposit;
@@ -115,12 +82,22 @@ contract PaymentAndRefund {
     }
 
     function buyerClaimRefund() external {
-        uint64 refund = _getEligibleWithdrawAmount(msg.sender);
+        uint64 refundInDollars = getEligibleRefundAmount(msg.sender);
 
-        depositedUSDC -= refund; 
+        depositedUSDC -= refundInDollars; 
         delete deposits[msg.sender];
 
-        //USDC.transfer(msg.sender, refund);
+        USDC.transfer(msg.sender, refundInDollars * 10 ** 6);
+    }
+
+//-----------------------------SELLER (admin)--------------------------------\\
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+
+    function setPrice(uint64 _price) external onlyAdmin {
+        priceInDollars = _price;
     }
 
     function setRefundSchedule(uint8[] calldata _schedule) external onlyAdmin {
@@ -138,48 +115,14 @@ contract PaymentAndRefund {
 
     // kick student from the program. Refund them according to what they are owed
     function sellerTerminateAgreement(address _student) external onlyAdmin {
-        uint64 refund = _getEligibleWithdrawAmount(_student);
+        /*
+           uint64 refundInDollars = 
 
-        depositedUSDC -= refund;
+        depositedUSDC -= refundInDollars;
         delete deposits[_student];
 
-        //USDC.transfer(_student, refund);
-    }
-
-    function getEligibleRefundAmount(address _buyer) public view returns(uint256) {
-        return uint256(_getEligibleWithdrawAmount(_buyer));
-    }
-
-    function _getEligibleWithdrawAmount(address _account) internal view returns(uint64) {
-        uint64 startTime = deposits[_account].startTime;
-        uint64 currentTime = uint64(block.timestamp);
-
-        uint64 weeksIn = (startTime - currentTime) % ONE_WEEK;
-        uint64 multiplier = deposits[_account].refundSchedule[weeksIn] / 100; // Convert to decimal
-        uint64 refund = deposits[_account].originalDepositInDollars * multiplier;
-        
-        return refund;
-    }
-
-    function sellerGetEligibleWithdrawAmount(address[] calldata _buyers) public view returns (uint256[] memory) {
-        uint256 len = _buyers.length;
-        uint256[] memory refunds;
-        
-        // This math works for user withdrawing -- confirm intentions here
-        // Would invert logic for amount that can be safely withdrawn by admin
-        for (uint256 i = 0; i < len; ) {
-            //refunds.push(uint256(_getEligibleWithdrawAmount(_buyers[i]));
-
-            unchecked {
-                ++i;
-            }
-        }
-
-        return refunds;
-    }
-
-    function _sellerWithdraw(address _deposit) internal {
-
+        USDC.transfer(_student, refundInDollars * 10 ** 6);
+        */
     }
 
     function sellerWithdraw(address[] calldata _deposits) external onlyAdmin {
@@ -190,9 +133,36 @@ contract PaymentAndRefund {
             }
         }
     }
+//------------------------------------UTILS------------------------------------\\
 
-    function getContractUSDCBalance() external returns(uint256) {
-        //return USDC.balanceOf(address(this));
-        return 5;
+    function getEligibleRefundAmount(address _buyer) public view returns(uint64) {
+        uint64 startTime = deposits[_buyer].startTime;
+        uint64 currentTime = uint64(block.timestamp);
+
+        uint64 weeksComplete = (startTime - currentTime) % ONE_WEEK;
+        uint64 multiplier = deposits[_buyer].refundSchedule[weeksComplete] / 100; 
+        uint64 refundInDollars = deposits[_buyer].originalDepositInDollars * multiplier;
+        
+        return refundInDollars;
+    }
+
+    function _getEligibleWithdrawAmount(address _account) internal view returns(uint64) {
+    }
+
+    function sellerGetEligibleWithdrawAmount(address[] calldata _buyers) public view returns (uint256[] memory) {
+        uint256 len = _buyers.length;
+        uint256[] memory refunds;
+        
+        for (uint256 i = 0; i < len; ) {
+            //refunds.push(uint256(_getEligibleWithdrawAmount(_buyers[i]));
+            unchecked {
+                ++i;
+            }
+        }
+        return refunds;
+    }
+
+    function _sellerWithdraw(address _deposit) internal {
+
     }
 }
