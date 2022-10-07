5a69f1814c7ee0a9a062da3f00f34757280a8513
MB <michael.dburke56@gmail.com>
2022-10-07 17:40:19 -0500

    Create constants (time in seconds since EPOCH) for manipulating time.

--- test/PaymentAndRefund.js
+++ test/PaymentAndRefund.js
@@ -11,7 +11,18 @@ const USDC_WHALE = '0x7713974908be4bed47172370115e8b1219f4a5f0';
 const PRICE_IN_DOLLARS = 5_000; 
 const PRICE_SIX_DECIMALS = 5_000_000_000;
 const SPENDING_MONEY = PRICE_IN_DOLLARS * 2 * 10**6;
-const JAN_FIRST = 1640998860; // Time stamp for 1/1/22 01:01:00 AM
+
+/* 
+ * These constants will be used with `helpers.time.increaseTo(<time since epoch>);
+ * JAN_FIRST -> Time stamp for 1/1/22 01:01:00 AM
+ * JAN_TENTH -> 1 week and 2 days after JAN_FIRST
+ * FEB_SEVENTH -> 5 weeks and 2 days after JAN_FIRST
+ * APRIL_EIGHTEENTH -> 15 weeks and 2 days after JAN_FIRST
+ */
+const JAN_FIRST = 1640998860;
+const JAN_TENTH = 1641776460;
+const FEB_SEVENTH = 1644195660;
+const APRIL_EIGHTEENTH = 1650243660;
 
 describe("PaymentAndRefund", function () {
     async function deployFixture() {
@@ -23,7 +34,6 @@ describe("PaymentAndRefund", function () {
         const usdcContract = new ethers.
             Contract(USDC_ADDRESS, ERC20_ABI, ethers.provider);
 
-        // Set initial state
         const whale = await ethers.getImpersonatedSigner(USDC_WHALE);
         await paymentContract.connect(admin).setPrice(PRICE_IN_DOLLARS);
         await usdcContract.connect(whale).transfer(user1.address, SPENDING_MONEY);
@@ -77,17 +87,99 @@ describe("PaymentAndRefund", function () {
         });
     });
 
-    xdescribe("Withdraw", function () {
-        it("User can withdraw according the `refundSchedule` (Pull over push)", async function () {
-            const { instance, admin } = await loadFixture(deployFixture);
+    describe("Withdraw", function () {
+        describe("Original refundSchedule", function () {
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK1", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+                
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
 
-            expect(true).to.equal(false);
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK5", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+                
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
+
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK15", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+               // This is week15 and according the refund schedule there should be 0 refund. 
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
+
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can be withdrawn from program by administrator (Push payment)", async function () {
+                const { instance, admin } = await loadFixture(deployFixture);
+
+                expect(true).to.equal(false);
+            });
         });
 
-        it("User can be withdrawn from program by administrator (Push payment)", async function () {
-            const { instance, admin } = await loadFixture(deployFixture);
+        describe("Updated refundSchedule", function () {
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK1", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+                
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
 
-            expect(true).to.equal(false);
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK5", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+                
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
+
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can withdraw according the `refundSchedule` (Pull over push) WEEK15", async function () {
+                const { paymentContract, usdcContract, user1 } = await loadFixture(
+                    deployFixture);
+               // This is week15 and according the refund schedule there should be 0 refund. 
+                const approval = await usdcContract.connect(user1)
+                    .approve(paymentContract.address, PRICE_IN_DOLLARS * 10 ** 6);
+                const payment1 = await paymentContract.connect(user1)
+                    .payUpfront(PRICE_IN_DOLLARS, JAN_FIRST);
+
+
+                expect(true).to.equal(false);
+            });
+
+            it("User can be withdrawn from program by administrator (Push payment)", async function () {
+                const { instance, admin } = await loadFixture(deployFixture);
+
+                expect(true).to.equal(false);
+            });
         });
     });
 
