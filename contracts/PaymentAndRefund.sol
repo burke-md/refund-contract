2f6e9cc9f04abe9692c6dd111b8acf844d31f485
MB <michael.dburke56@gmail.com>
2022-10-12 09:26:37 -0500

    Lint w/ prettier-solidity.

--- contracts/PaymentAndRefund.sol
+++ contracts/PaymentAndRefund.sol
@@ -5,10 +5,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 pragma solidity 0.8.17;
 
 contract PaymentAndRefund {
-
     IERC20 USDC;
     uint8[15] public refundSchedule = [
-        100,100,75,75,75,75,50,50,50,50,25,25,25,25,0
+        100,
+        100,
+        75,
+        75,
+        75,
+        75,
+        50,
+        50,
+        50,
+        50,
+        25,
+        25,
+        25,
+        25,
+        0
     ];
 
     uint64 public priceInDollars;
@@ -16,10 +29,10 @@ contract PaymentAndRefund {
     mapping(address => Deposit) public deposits;
     address public admin;
     uint256 public constant MAX_TIME_FROM_START = 30 days;
-    uint256 public constant USDC_DECIMALS = 10**6; 
+    uint256 public constant USDC_DECIMALS = 10**6;
 
     struct Deposit {
-        uint64 originalDepositInDollars;  
+        uint64 originalDepositInDollars;
         uint64 balanceInDollars;
         uint64 depositTime;
         uint64 startTime;
@@ -31,28 +44,41 @@ contract PaymentAndRefund {
         USDC = IERC20(_usdc);
     }
 
-    modifier onlyAdmin {
+    modifier onlyAdmin() {
         require(msg.sender == admin, "onlyAdmin");
         _;
     }
-    
+
     function payUpfront(uint64 _price, uint64 _startTime) external {
         uint64 currentPriceInDollars = priceInDollars;
         uint64 currentTime = uint64(block.timestamp);
-       
+
         uint256 maxPastTime = currentTime - MAX_TIME_FROM_START;
         uint256 maxFutureTime = currentTime + MAX_TIME_FROM_START;
 
-        require(_startTime >= maxPastTime && _startTime <= maxFutureTime,
-                "User must select start time within bounds.");
-        require(deposits[msg.sender].depositTime == 0,
-                "User cannot deposit twice.");
-        require(_price == currentPriceInDollars,
-                "User must pay correct price.");
-        require(USDC.allowance(msg.sender, address(this)) >= currentPriceInDollars * USDC_DECIMALS,
-                "User must have made allowance via USDC contract.");
-                
-        USDC.transferFrom(msg.sender, address(this), currentPriceInDollars * USDC_DECIMALS);
+        require(
+            _startTime >= maxPastTime && _startTime <= maxFutureTime,
+            "User must select start time within bounds."
+        );
+        require(
+            deposits[msg.sender].depositTime == 0,
+            "User cannot deposit twice."
+        );
+        require(
+            _price == currentPriceInDollars,
+            "User must pay correct price."
+        );
+        require(
+            USDC.allowance(msg.sender, address(this)) >=
+                currentPriceInDollars * USDC_DECIMALS,
+            "User must have made allowance via USDC contract."
+        );
+
+        USDC.transferFrom(
+            msg.sender,
+            address(this),
+            currentPriceInDollars * USDC_DECIMALS
+        );
 
         unchecked {
             depositedUSDC += currentPriceInDollars;
@@ -73,7 +99,7 @@ contract PaymentAndRefund {
     function buyerClaimRefund() external {
         uint256 refundInDollars = calculateRefundDollars(msg.sender);
 
-        depositedUSDC -= refundInDollars; 
+        depositedUSDC -= refundInDollars;
         delete deposits[msg.sender];
 
         USDC.transfer(msg.sender, refundInDollars * USDC_DECIMALS);
@@ -83,14 +109,26 @@ contract PaymentAndRefund {
         priceInDollars = _price;
     }
 
-    function setRefundSchedule(uint8[15] calldata _schedule) external onlyAdmin {
-        require(_schedule[0] > 0, "must have at least 1 non-zero refund period");
-        require(_schedule[_schedule.length - 1] == 0, "must end with zero refund");
+    function setRefundSchedule(uint8[15] calldata _schedule)
+        external
+        onlyAdmin
+    {
+        require(
+            _schedule[0] > 0,
+            "must have at least 1 non-zero refund period"
+        );
+        require(
+            _schedule[_schedule.length - 1] == 0,
+            "must end with zero refund"
+        );
 
-        uint len = _schedule.length;
+        uint256 len = _schedule.length;
         for (uint256 i = 0; i < len - 1; ) {
             uint256 idxValue = _schedule[i];
-            require(idxValue >= _schedule[i + 1], "refund must be non-increasing");
+            require(
+                idxValue >= _schedule[i + 1],
+                "refund must be non-increasing"
+            );
             require(idxValue < 101, "refund cannot exceed 100%");
             unchecked {
                 ++i;
@@ -125,13 +163,17 @@ contract PaymentAndRefund {
         USDC.transfer(admin, dollarsToWithdraw * USDC_DECIMALS);
     }
 
-    function calculateRefundDollars(address _buyer) public view returns(uint256) {
+    function calculateRefundDollars(address _buyer)
+        public
+        view
+        returns (uint256)
+    {
         uint256 paidDollars = deposits[_buyer].originalDepositInDollars;
         uint256 scheduleLength = deposits[_buyer].refundSchedule.length;
 
         uint256 weeksComplete = _getWeeksComplete(_buyer);
         uint256 multiplier;
-       
+
         if (weeksComplete < scheduleLength) {
             multiplier = deposits[_buyer].refundSchedule[weeksComplete];
         }
@@ -143,32 +185,42 @@ contract PaymentAndRefund {
         return (paidDollars * multiplier) / 100;
     }
 
-
-    function calculateSafeWithdrawDollars(address _buyer) public view returns(uint256) {
+    function calculateSafeWithdrawDollars(address _buyer)
+        public
+        view
+        returns (uint256)
+    {
         uint256 accountRequirments = calculateRefundDollars(_buyer);
         uint256 accountBalance = deposits[_buyer].balanceInDollars;
 
         if (accountRequirments >= accountBalance) {
             return 0;
         }
-        
+
         return accountBalance - accountRequirments;
     }
 
-    function _getWeeksComplete(address _account) internal view returns(uint256) {
+    function _getWeeksComplete(address _account)
+        internal
+        view
+        returns (uint256)
+    {
         uint256 startTime = deposits[_account].startTime;
         uint256 currentTime = block.timestamp;
-        
-        if (currentTime < startTime) { 
+
+        if (currentTime < startTime) {
             return 0;
         }
 
         uint256 weeksComplete = (currentTime - startTime) / 1 weeks;
-        
+
         return weeksComplete;
     }
 
-    function rescueERC20Token(IERC20 _tokenContract, uint256 _amount) external onlyAdmin {
+    function rescueERC20Token(IERC20 _tokenContract, uint256 _amount)
+        external
+        onlyAdmin
+    {
         _tokenContract.transfer(admin, _amount);
     }
 }
